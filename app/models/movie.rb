d6034a717c5c0003657688fb21e489380198d511
andrewheppner <andrewbheppner@gmail.com>
2015-06-02 22:22:12 +0000

    add image upload feature

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,21 +2,23 @@ class Movie < ActiveRecord::Base
 
   has_many :reviews
 
-validates :title, :director, :description, :poster_image_url, :release_date, presence: true
-validates :runtime_in_minutes, numericality: { only_integer: true }
-validate :release_date_is_in_the_future
+  validates :title, :director, :description, :release_date, presence: true
+  validates :runtime_in_minutes, numericality: { only_integer: true }
+  validate :release_date_is_in_the_future
 
-def review_average
-  reviews.sum(:rating_out_of_ten)/reviews.size if reviews.size > 0
-end
+  mount_uploader :image, ImageUploader
 
-protected
-
-def release_date_is_in_the_future
-  if release_date
-    errors.add(:release_date, "should be in the future") if release_date < Date.today
+  def review_average
+    reviews.sum(:rating_out_of_ten)/reviews.size if reviews.size > 0
   end
-end
+
+  protected
+
+  def release_date_is_in_the_future
+    if release_date
+      errors.add(:release_date, "should be in the future") if release_date < Date.today
+    end
+  end
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602193213) do
+ActiveRecord::Schema.define(version: 20150602220105) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150602193213) do
     t.datetime "release_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "reviews", force: true do |t|
