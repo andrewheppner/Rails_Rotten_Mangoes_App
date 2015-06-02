39702ba94e66826edd4f1d811785b3f6ca2e9642
andrewheppner <andrewbheppner@gmail.com>
2015-06-02 15:48:11 +0000

    migrate movies table

---
+++ db/migrate/20150602154700_create_movies.rb
@@ -0,0 +1,19 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :rails
+      t.string :g
+      t.string :model
+      t.string :movie
+      t.string :title
+      t.string :director
+      t.integer :runtime_in_minutes
+      t.text :description
+      t.string :poster_image_url
+      t.datetime :release_date
+
+      t.timestamps
+    end
+  end
+end
